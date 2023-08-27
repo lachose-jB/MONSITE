966b9e2aa626edf49578b2dd08509d851b513f10
lachose-jB <jadjahouisso@gmail.com>
2023-08-27 17:16:37 +0200

    Mise a jour des competences

--- js/mouse.js
+++ js/mouse.js
@@ -1,3 +1,44 @@
+
+//Expérience et formation
+let tabFormaElement=[];
+let tabFormaClassName=[];
+let tabFormaTextElement=[];
+let activeText;
+let NbDetailles= 10
+for(let i = 0; i<=NbDetailles; i++)
+{
+    tabFormaElement[i]= document.getElementById("Vp"+ i);
+    tabFormaClassName[i]= "#Vp"+i;
+    tabFormaTextElement[i]= "#DetSec"+i;
+}
+
+for(let i = 0; i<=tabFormaElement.length; i++)
+{
+    tabFormaElement[i].addEventListener("click",mySkils);
+
+    function mySkils() {
+        var elem = $(""+tabFormaClassName[i]).text();
+        if (elem == "+") {
+          //Stuff to do when btn is in the Skils + state
+          $(""+tabFormaClassName[i]).text("-");
+          $(""+tabFormaTextElement[i]).slideDown();
+          if (activeText != i){
+            $(""+tabFormaTextElement[activeText]).slideUp();
+            $(""+tabFormaClassName[activeText]).text("+");
+          }
+          activeText = i
+        } else {
+          //Stuff to do when btn is in the Skils - state
+          $(""+tabFormaClassName[i]).text("+");
+          $(""+tabFormaTextElement[i]).slideUp();
+        }
+      }
+}
+// SOURISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+// SOURISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+// SOURISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+// SOURISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+// SOURISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 const cursor = document.getElementById("cursor");
 const amount = 20;
 const sineDots = Math.floor(amount * 0.3);
@@ -195,39 +236,3 @@ const positionCursor = (delta) => {
 };
 
 init();
-
-//Expérience et formation
-let tabFormaElement=[];
-let tabFormaClassName=[];
-let tabFormaTextElement=[];
-let activeText;
-let NbDetailles= 10
-for(let i = 0; i<=NbDetailles; i++)
-{
-    tabFormaElement[i]= document.getElementById("Vp"+ i);
-    tabFormaClassName[i]= "#Vp"+i;
-    tabFormaTextElement[i]= "#DetSec"+i;
-}
-
-for(let i = 0; i<=tabFormaElement.length; i++)
-{
-    tabFormaElement[i].addEventListener("click",mySkils);
-
-    function mySkils() {
-        var elem = $(""+tabFormaClassName[i]).text();
-        if (elem == "+") {
-          //Stuff to do when btn is in the Skils + state
-          $(""+tabFormaClassName[i]).text("-");
-          $(""+tabFormaTextElement[i]).slideDown();
-          if (activeText != i){
-            $(""+tabFormaTextElement[activeText]).slideUp();
-            $(""+tabFormaClassName[activeText]).text("+");
-          }
-          activeText = i
-        } else {
-          //Stuff to do when btn is in the Skils - state
-          $(""+tabFormaClassName[i]).text("+");
-          $(""+tabFormaTextElement[i]).slideUp();
-        }
-      }
-}
